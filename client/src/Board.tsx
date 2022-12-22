c37d201525da4399f640a19b50644ef2ab648718
floyrin <janedqwang@gmail.com>
2022-12-22 19:53:26 +1100

    added board and logic for mine/flag/free cells

---
+++ client/src/Board.tsx
@@ -0,0 +1,32 @@
+import { BoardAttributes, CellAttributes } from './Helpers';
+import BoardRow from './BoardRow';
+
+const Board = ({ difficulty }: BoardAttributes) => {
+  let size = difficulty === 0 ? 8 : difficulty === 1 ? 14 : 20;
+  let board: CellAttributes[][] = new Array(size)
+    .fill({ hidden: true, value: 0 })
+    .map(() => new Array(size).fill({ hidden: true, value: 0 }));
+
+  const generateBoard = () => {
+    for (let x = 0; x < size; x++) {
+      for (let y = 0; y < size; y++) {
+        if (x % 2 == 1) {
+          board[x][y].value = -1;
+        } else {
+          board[x][y].value = 1;
+        }
+      }
+    }
+    return board;
+  };
+
+  return (
+    <>
+      {generateBoard().map((row: CellAttributes[], idx) => {
+        return <BoardRow key={idx} row={row} />;
+      })}
+    </>
+  );
+};
+
+export default Board;
